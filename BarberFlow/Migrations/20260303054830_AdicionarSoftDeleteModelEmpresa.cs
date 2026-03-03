030b55221af593b9d17d4a4889212ece575b2637
Henrique Vasques <henriquevasques04@gmail.com>
2026-03-03 19:30:30 -0300

    Fazendo atualização na model Empresa.cs, adicionei duas novas propiedades, aAtivo e IsDeleted, atualizando o banco com as migrations.

---
+++ BarberFlow/Migrations/20260303054830_AdicionarSoftDeleteModelEmpresa.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberFlow.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarSoftDeleteModelEmpresa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Empresas",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Ativo",
+                table: "Empresas",
+                type: "boolean",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Empresas",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresas_CNPJ",
+                table: "Empresas",
+                column: "CNPJ",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Empresas_CNPJ",
+                table: "Empresas");
+
+            migrationBuilder.DropColumn(
+                name: "Ativo",
+                table: "Empresas");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Empresas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Empresas",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
--- BarberFlow/Models/Empresa.cs
+++ BarberFlow/Models/Empresa.cs
@@ -1,36 +1,38 @@
-﻿using BarberFlow.API.Helpers;
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
+﻿    using BarberFlow.API.Helpers;
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
 
-namespace BarberFlow.API.Models
-{
-    [Index(nameof(Slug), IsUnique = true)]
-    public class Empresa
+    namespace BarberFlow.API.Models 
     {
-        #region Properties
-        public long Id { get; set; }
-        [Required, MaxLength(150)]
-        public string Nome { get; set; }
-        [Required, MaxLength(150)]
-        public string Slug { get; set; }
-        public string CNPJ { get; set; }
-        public DateTime DataCriacao { get; set; }
-        public DateTime DataAtualizacao { get; set; }
-        public ICollection<Usuario> Usuarios { get; set; }
-        public ICollection<Servico> Servicos { get; set; }
-        #endregion
-
-        #region Constructors
-        public Empresa(string nome, string cnpj)
+        [Index(nameof(Slug), IsUnique = true)]
+        [Index(nameof(CNPJ), IsUnique = true)]
+        public class Empresa
         {
-            Nome = nome;
-            CNPJ = cnpj;
-            DataCriacao = DateTime.UtcNow;
-            DataAtualizacao = DateTime.UtcNow;
-            //Ativo = true;
-        }
+            #region Properties
+            public long Id { get; set; }
+            [Required, MaxLength(150)]
+            public string Nome { get; set; }
+            [Required, MaxLength(150)]
+            public bool Ativo { get; set; } = true; 
+            public bool IsDeleted { get; set; } = false;
+            public string Slug { get; set; }
+            public string CNPJ { get; set; }
+            public DateTime DataCriacao { get; set; }
+            public DateTime DataAtualizacao { get; set; }
+            public ICollection<Usuario> Usuarios { get; set; }
+            public ICollection<Servico> Servicos { get; set; }
+            #endregion
 
-        protected Empresa() {}
-        #endregion
+            #region Constructors
+            public Empresa(string nome, string cnpj)
+            {
+                Nome = nome;
+                CNPJ = cnpj;
+                DataCriacao = DateTime.UtcNow;
+                DataAtualizacao = DateTime.UtcNow;
+            }
+
+            protected Empresa() {}
+            #endregion
+        }
     }
-}
