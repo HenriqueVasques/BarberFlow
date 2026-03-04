4d2ece38c4238013dae45b729f853e582a885557
Henrique Vasques <henriquevasques04@gmail.com>
2026-03-04 00:28:40 -0300

    Criando métodos, atualizar e deletar, mudando o filtro para não trazer servico deletados e inativos no método ObterPorEmpresa, ccriando um método ObterPorEmpresaAdmin sem esses filtros apenas para me lembrar de implementar ele no futuro.

--- BarberFlow/Data/Repositories/ServicoRepository.cs
+++ BarberFlow/Data/Repositories/ServicoRepository.cs
@@ -17,12 +17,39 @@
                 await _appDbContext.Servicos.AddAsync(servico);
                 await _appDbContext.SaveChangesAsync();
             }
+
+            public async Task Atualizar(Servico servico)
+            {
+                _appDbContext.Servicos.Update(servico);
+                await _appDbContext.SaveChangesAsync();
+            }
+
+            public async Task Deletar(Servico servico)
+            {
+                _appDbContext.Servicos.Update(servico);
+                await _appDbContext.SaveChangesAsync();
+            }
+
             public async Task<IEnumerable<Servico>> ObterPorEmpresa(long empresaId)
+            {
+                return await _appDbContext.Servicos
+                .Where(s => s.EmpresaId == empresaId && !s.IsDeleted && s.Ativo)
+                .AsNoTracking()
+                .ToListAsync();
+            }
+
+            public async Task<IEnumerable<Servico>> ObterPorEmpresaAdmin(long empresaId)
             {
                 return await _appDbContext.Servicos
                 .Where(s => s.EmpresaId == empresaId)
                 .AsNoTracking()
                 .ToListAsync();
+            }
+
+        public async Task<Servico?> ObterPorId(long id)
+            {
+                return await _appDbContext.Servicos
+                .FirstOrDefaultAsync(s => s.Id == id && !s.IsDeleted);
             }
         }
     }
