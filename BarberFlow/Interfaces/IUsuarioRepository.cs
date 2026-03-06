a25f1f15a3d87067dd052b3ba362c0e14aaab48c
Henrique Vasques <henriquevasques04@gmail.com>
2026-03-06 04:26:33 -0300

    Alterando o retorno de alguns metodos do arquivo IUsuarioRepository.cs.

---
+++ BarberFlow/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using BarberFlow.API.DTOs.Usuario;
+using BarberFlow.API.Models;
+
+namespace BarberFlow.API.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        public Task Adicionar(Usuario usuario);
+        public Task<Usuario?> ObterPorId(long id);
+        public Task Atualizar(Usuario usuario);
+        public Task Deletar(Usuario usuario);
+        public Task<bool> ExisteEmail(string email);
+        public Task AlterarSenha(long id, string senha);
+    }
+}
