6bd604613c0257416e8b47a5d61c94fa18e205fb
Henrique Vasques <henriquevasques04@gmail.com>
2026-03-18 05:26:59 -0300

    Removendo DataAtualizacao do UsuarioAlterarSenhaDto, e colocando required na propiedade Senha.

--- BarberFlow/DTOs/Usuario/UsuarioAlterarSenhaDto.cs
+++ BarberFlow/DTOs/Usuario/UsuarioAlterarSenhaDto.cs
@@ -5,7 +5,6 @@ namespace BarberFlow.API.DTOs.Usuario
     public class UsuarioAlterarSenhaDto
     {
         [MinLength(8, ErrorMessage = "A senha deve ter no mínimo 8 caracteres.")]
-        public string Senha { get; set; }
-        public DateTime DataAtualizacao { get; set; }
+        public required string Senha { get; set; }
     }
 }
