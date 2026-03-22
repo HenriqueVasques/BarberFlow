fbc1f2cc89ac6c4ff9f5762a249afb345ccaf0ea
Henrique Vasques <henriquevasques04@gmail.com>
2026-03-22 08:00:00 -0300

    Criando esse dto pra transportar exibir dados que são buscados com os novos métodos get, para trazer ao usuario um resumo financeiro do dia escolhido.

---
+++ BarberFlow/DTOs/Agendamento/DashboardResumoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberFlow.API.DTOs.Agendamento
+{
+    public class DashboardResumoDto
+    {
+        public DateTime Data { get; set; }
+        public decimal FaturamentoTotal { get; set; }
+        public int QuantidadeAtendimentos { get; set; }
+
+    }
+}
