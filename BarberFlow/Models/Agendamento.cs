ade30aff71d582671497d9ef6e692b140567c29c
Henrique Vasques <henriquevasques04@gmail.com>
2026-03-26 10:17:44 -0300

    Removendo ProfissionalId e ServicoId, para colocar ProfissionalServicoId, na model Agendamento.

--- BarberFlow/Models/Agendamento.cs
+++ BarberFlow/Models/Agendamento.cs
@@ -8,21 +8,19 @@ namespace BarberFlow.API.Models
         public long Id { get; set; }
         public long EmpresaId { get; set; }
         public long ClienteId { get; set; }
-        public long ProfissionalId { get; set; }
-        public long ServicoId { get; set; }
+        public long ProfissionalServicoId { get; set; }
         public DateTime DataHoraInicio { get; set; }
         public DateTime DataHoraFim { get; set; }
         public StatusAgendamento Status { get; set; } = StatusAgendamento.Pendente;
         public int ?FormaPagamento { get; set; }
         public decimal PrecoNoMomento { get; set; }
-        public DateTime DataCriacao { get; set; }
-        public DateTime DataAtualizacao { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+        public DateTime DataAtualizacao { get; set; } = DateTime.UtcNow;
 
         //navegation properties
         public Empresa Empresa { get; set; }
         public Cliente Cliente { get; set; }
-        public Profissional Profissional { get; set; }
-        public Servico Servico { get; set; }
+        public ProfissionalServico ProfissionalServico { get; set; }
 
 
     }
