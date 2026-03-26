168efde79f16506411e21126b00361066cb8d5cd
Henrique Vasques <henriquevasques04@gmail.com>
2026-03-26 09:59:15 -0300

    Criando novo DTO para mapear os objetos no meu repository, para não buscar no banco um objeto grande e pesado cheio de join, com informações desnecessárias, agora ele preenche esse DTO com somente o necessário, apenas para métodos de exibição, onde eu nao vou manipular objetos, nesse caso, ObterPorId segue com includes, e retornando o objeto Agendamento.

---
+++ BarberFlow/DTOs/Agendamento/AgendamentoDetalhesDto.cs
@@ -0,0 +1,23 @@
+﻿using BarberFlow.API.Enums;
+
+public class AgendamentoDetalhesDto
+{
+    public long Id { get; set; }
+    public long ProfissionalId { get; set; }
+    public long ClienteId { get; set; }
+    public long ServicoId { get; set; }
+
+    public string NomeProfissional { get; set; }
+    public string NomeCliente { get; set; }
+    public string NomeServico { get; set; }
+    public string NomeEmpresa { get; set; }
+
+    // Valores e Tempos
+    public decimal PrecoNoMomento { get; set; }
+    public DateTime DataHoraInicio { get; set; }
+    public DateTime DataHoraFim { get; set; }
+
+    // Status (Enum)
+    public StatusAgendamento Status { get; set; }
+    public string StatusDescricao => Status.ToString(); 
+}
