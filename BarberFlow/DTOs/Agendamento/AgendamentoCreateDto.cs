a4d9125acc66ffa787b9aeca1110484235c6bb88
Henrique Vasques <henriquevasques04@gmail.com>
2026-03-26 10:16:09 -0300

    Nos DTOs create e reponse, removi ServicoId e ProfissionalId para colocar ServicoProfissionalId

--- BarberFlow/DTOs/Agendamento/AgendamentoCreateDto.cs
+++ BarberFlow/DTOs/Agendamento/AgendamentoCreateDto.cs
@@ -3,9 +3,8 @@
     public class AgendamentoCreateDto
     {
         public long EmpresaId { get; set; }
-        public long ProfissionalId { get; set; }
         public long ClienteId { get; set; }
-        public long ServicoId { get; set; }
+        public long ProfissionalServicoId { get; set; }
         public DateTime DataHoraInicio { get; set; }
         //public int FormaPagamento { get; set; }
     }
--- BarberFlow/DTOs/Agendamento/AgendamentoResponseDto.cs
+++ BarberFlow/DTOs/Agendamento/AgendamentoResponseDto.cs
@@ -10,9 +10,8 @@ namespace BarberFlow.API.DTOs.Agendamento
         public string NomeServico { get; set; }
         public string NomeEmpresa { get; set; }
         public long EmpresaId { get; set; }
-        public long ProfissionalId { get; set; }
         public long ClienteId { get; set; }
-        public long ServicoId { get; set; }
+        public long ProfissionalServicoId { get; set; }
         public decimal PrecoNoMomento { get; set; }
         public DateTime DataHoraInicio { get; set; }
         public DateTime DataHoraFim { get; set; }
