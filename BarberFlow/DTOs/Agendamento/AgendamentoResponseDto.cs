9747e936cdb737e8a114095152dc227270fb485f
Henrique Vasques <henriquevasques04@gmail.com>
2026-04-03 02:24:42 -0300

    Organizando o código com regions e ajustando dto para retornar todos os campos necessários e atender o repository.

--- BarberFlow/DTOs/Agendamento/AgendamentoResponseDto.cs
+++ BarberFlow/DTOs/Agendamento/AgendamentoResponseDto.cs
@@ -2,21 +2,35 @@
 
 namespace BarberFlow.API.DTOs.Agendamento
 {
-    internal class AgendamentoResponseDto
+    public class AgendamentoResponseDto
     {
+        #region Identificadores (Chaves e Relacionamentos)
         public long Id { get; set; }
+        public long EmpresaId { get; set; }
+        public long ClienteId { get; set; }
+        public long ServicoId { get; set; }
+        public long ProfissionalId { get; set; }
+        public long ProfissionalServicoId { get; set; }
+        #endregion
+
+        #region Informações de Exibição (Nomes)
         public string NomeProfissional { get; set; }
         public string NomeCliente { get; set; }
         public string NomeServico { get; set; }
         public string NomeEmpresa { get; set; }
-        public long EmpresaId { get; set; }
-        public long ClienteId { get; set; }
-        public long ProfissionalServicoId { get; set; }
+        #endregion
+
+        #region Valores e Status
         public decimal PrecoNoMomento { get; set; }
+        public StatusAgendamento Status { get; set; }
+        public string StatusDescricao => Status.ToString();
+        #endregion
+
+        #region Datas e Auditoria
         public DateTime DataHoraInicio { get; set; }
         public DateTime DataHoraFim { get; set; }
-        public StatusAgendamento Status { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataAtualizacao { get; set; }
+        #endregion
     }
 }
