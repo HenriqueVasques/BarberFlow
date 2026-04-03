3a046384630fb17fe59d6062acaf21a86fa04178
Henrique Vasques <henriquevasques04@gmail.com>
2026-04-03 02:29:38 -0300

    Organizando o código e colocando alguns campos como nullable.

--- BarberFlow/DTOs/BloqueioHorario/BloqueioHorarioResponseDto.cs
+++ BarberFlow/DTOs/BloqueioHorario/BloqueioHorarioResponseDto.cs
@@ -1,17 +1,24 @@
-﻿
-namespace BarberFlow.API.Controllers
+﻿namespace BarberFlow.API.Controllers
 {
-    internal class BloqueioHorarioResponseDto
+    public class BloqueioHorarioResponseDto
     {
+        #region Identificadores (Chaves e Relacionamentos)
         public long Id { get; set; }
-        public string NomeProfissional { get; set; }
-        public string NomeEmpresa { get; set; }
         public long EmpresaId { get; set; }
         public long ProfissionalId { get; set; }
+        #endregion
+
+        #region Informações de Exibição
+        public string? NomeProfissional { get; set; }
+        public string? NomeEmpresa { get; set; }
+        public string Motivo { get; set; }
+        #endregion
+
+        #region Período e Auditoria
         public DateTime DataHoraInicio { get; set; }
         public DateTime DataHoraFim { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataAtualizacao { get; set; }
-        public string Motivo { get; set; }
+        #endregion
     }
 }
