3fa4f027a0de3df6bf16b880457ba8407e2d0ef8
Henrique Vasques <henriquevasques04@gmail.com>
2026-04-03 03:22:00 -0300

    Organizando com regions.

--- BarberFlow/Models/Agendamento.cs
+++ BarberFlow/Models/Agendamento.cs
@@ -1,10 +1,10 @@
 ﻿using BarberFlow.API.Enums;
-using System.Numerics;
 
 namespace BarberFlow.API.Models
 {
     public class Agendamento
     {
+        #region Propriedades de Persistência (Banco de Dados)
         public long Id { get; set; }
         public long EmpresaId { get; set; }
         public long ClienteId { get; set; }
@@ -12,16 +12,19 @@ namespace BarberFlow.API.Models
         public DateTime DataHoraInicio { get; set; }
         public DateTime DataHoraFim { get; set; }
         public StatusAgendamento Status { get; set; } = StatusAgendamento.Pendente;
-        public int ?FormaPagamento { get; set; }
+        public int? FormaPagamento { get; set; }
         public decimal PrecoNoMomento { get; set; }
+        #endregion
+
+        #region Auditoria e Controle
         public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
         public DateTime DataAtualizacao { get; set; } = DateTime.UtcNow;
+        #endregion
 
-        //navegation properties
-        public Empresa Empresa { get; set; }
-        public Cliente Cliente { get; set; }
-        public ProfissionalServico ProfissionalServico { get; set; }
-
-
+        #region Propriedades de Navegação (Relacionamentos)
+        public virtual Empresa Empresa { get; set; }
+        public virtual Cliente Cliente { get; set; }
+        public virtual ProfissionalServico ProfissionalServico { get; set; }
+        #endregion
     }
 }
