cc10b3b5ce182ece56e32ae973d9e506c0c39318
Henrique Vasques <henriquevasques04@gmail.com>
2026-04-03 07:14:47 -0300

    Ajustando os métodos para funcionarem de maneira dinamica, organizando codigo com regions.

--- BarberFlow/Interfaces/IClienteRepository.cs
+++ BarberFlow/Interfaces/IClienteRepository.cs
@@ -1,14 +1,24 @@
-﻿using BarberFlow.API.Data.Repositories;
+﻿using BarberFlow.API.DTOs.Cliente;
 using BarberFlow.API.Models;
 
 namespace BarberFlow.API.Interfaces
 {
     public interface IClienteRepository
     {
+        #region Operações de Escrita
+
         Task Adicionar(Cliente cliente);
         Task Atualizar(Cliente cliente);
         Task Deletar(Cliente cliente);
-        Task<Cliente?> ObterPorId(long id);
-        Task<IEnumerable<Cliente>> ObterPorEmpresa(long empresaId);
+
+        #endregion
+
+        #region Operações de Leitura
+
+        Task<Cliente?> ObterPorId(long id, bool incluirDeletados = false);
+        Task<Cliente?> ObterClienteComUsuario(long id);
+        Task<IEnumerable<ClienteResponseDto>> ObterPorEmpresa(long empresaId, bool incluirDeletados = false, int pagina = 1);
+
+        #endregion
     }
 }
