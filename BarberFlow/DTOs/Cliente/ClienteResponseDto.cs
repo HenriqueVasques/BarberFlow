bc8896ed4209ab1e0b9d732f07bc3e57005954c9
Henrique Vasques <henriquevasques04@gmail.com>
2026-04-03 07:11:50 -0300

    Organizando com regions e comentários, adicionando nullable a alguns campos.

--- BarberFlow/DTOs/Cliente/ClienteResponseDto.cs
+++ BarberFlow/DTOs/Cliente/ClienteResponseDto.cs
@@ -2,16 +2,24 @@
 {
     public class ClienteResponseDto
     {
+        #region Identificadores e Vínculos
         public long Id { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Telefone { get; set; }
-        public string Whatsapp { get; set; }
         public long EmpresaId { get; set; }
         public long UsuarioId { get; set; }
+        #endregion
+
+        #region Informações de Contato
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone { get; set; }
+        public string? Whatsapp { get; set; }
+        #endregion
+
+        #region Status e Auditoria
+        public bool Ativo { get; set; }
+        public bool IsDeleted { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataAtualizacao { get; set; }
-        public bool IsDeleted { get; set; }
-        public bool Ativo { get; set; }
+        #endregion
     }
 }
