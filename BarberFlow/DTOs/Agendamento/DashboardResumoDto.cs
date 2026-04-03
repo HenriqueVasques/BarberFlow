18dc59da82a4a8d92fd9be80c9a5050dd81a5fdd
Henrique Vasques <henriquevasques04@gmail.com>
2026-04-03 02:26:45 -0300

    Alterando tipo da propiedade Data, e organizando código.

--- BarberFlow/DTOs/Agendamento/DashboardResumoDto.cs
+++ BarberFlow/DTOs/Agendamento/DashboardResumoDto.cs
@@ -2,9 +2,13 @@
 {
     public class DashboardResumoDto
     {
+        #region Identificação do Período
         public DateOnly Data { get; set; }
+        #endregion
+
+        #region Indicadores de Desempenho (Métricas)
         public decimal FaturamentoTotal { get; set; }
         public int QuantidadeAtendimentos { get; set; }
-
+        #endregion
     }
 }
