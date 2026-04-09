48e87864dc863e81f6c199500461c1f7e8675e36
Henrique Vasques <henriquevasques04@gmail.com>
2026-04-08 22:34:04 -0300

    Organizado em regions.

--- BarberFlow/DTOs/Empresa/EmpresaResponseDto.cs
+++ BarberFlow/DTOs/Empresa/EmpresaResponseDto.cs
@@ -2,14 +2,21 @@
 {
     public class EmpresaResponseDto
     {
+        #region Dados de Identificação
         public long Id { get; set; }
         public string Nome { get; set; }
         public string Slug { get; set; }
         public string CNPJ { get; set; }
+        #endregion
+
+        #region Status e Controle
+        public bool Ativo { get; set; }
+        public bool IsDeleted { get; set; }
+        #endregion
+
+        #region Datas de Auditoria
         public DateTime DataCriacao { get; set; }
         public DateTime DataAtualizacao { get; set; }
-        public bool IsDeleted { get; set; }
-        public bool Ativo { get; set; }
-
+        #endregion
     }
 }
